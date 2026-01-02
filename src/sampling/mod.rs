5048e14527e83b0ff8216e7b0373317ac5b994bd
Victor Zhang <zxjzxj9@gmail.com>
2026-01-02 17:34:52 +0800

    upadte sampling

--- src/sampling/mod.rs
+++ src/sampling/mod.rs
@@ -3,7 +3,10 @@
 mod traits;
 mod vmc;
 mod dmc;
+mod pimc;
 
 pub use traits::{EnergyCalculator, Walker, BranchingResult, VmcWalker};
 pub use vmc::{MCMCParams, MCMCState, MCMCResults, MCMCSimulation};
 pub use dmc::{run_dmc_sampling, HarmonicWalker, HydrogenAtomWalker, HydrogenMoleculeWalker};
+pub use pimc::{QuantumPath, PIMCSimulation, run_pimc_harmonic};
+
