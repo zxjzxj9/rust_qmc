565f0efe2b4bfe9e1495f0556fd819e1af5df1dc
Victor Zhang <zxjzxj9@gmail.com>
2026-01-08 21:59:15 +0800

    update module for fermion

--- src/sampling/mod.rs
+++ src/sampling/mod.rs
@@ -4,6 +4,7 @@ mod traits;
 mod vmc;
 mod dmc;
 mod pimc;
+mod pimc_fermion;
 
 pub use traits::{EnergyCalculator, Walker, BranchingResult, VmcWalker};
 pub use vmc::{MCMCParams, MCMCState, MCMCResults, MCMCSimulation};
@@ -13,4 +14,7 @@ pub use pimc::{
     // Generalized potentials
     Potential, HarmonicPotential, SombreroPotential, DoubleWellPotential,
     GeneralPath, GeneralPIMC, run_pimc_sombrero,
+};
+pub use pimc_fermion::{
+    TrialWavefunction, Hydrogen1s, FermionPath, FermionPIMC, run_pimc_hydrogen,
 };
